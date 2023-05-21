1e41302e71690a16f8de2e96d97737447072385b
Garrett <gnjulaka@gmail.com>
2023-05-21 01:41:23 -0500

    Going to space as leader

--- nvim/init.lua
+++ nvim/init.lua
@@ -72,12 +72,16 @@ vim.o.tgc = true
 vim.o.textwidth = 80
 vim.o.shiftwidth = 2 
 vim.o.tabstop = 2 
+ vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
+
+ vim.g.mapleader = " "
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-q>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>T', '<cmd>TestFile<cr>', {})
 
 local lspconfig = require("lspconfig")
 
