34971ae79fe445bf379fbb20aac6221f5e00106f
Garrett <duldonkulus@gmail.com>
2024-03-31 18:54:33 -0500

    Add some obsidian keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,14 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim" if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -38,17 +40,17 @@ vim.keymap.set("n", "<leader>T", "<cmd>TestFile<CR>")
 vim.keymap.set("n", "<leader>N", "<cmd>TestNearest<CR>")
 vim.keymap.set("n", "<leader>q", "<cmd>VimuxCloseRunner<CR>")
 vim.keymap.set("n", "<leader>g", "<cmd>VimuxOpenRunner<CR>")
-vim.keymap.set("n", "<leader>r", "<cmd>Telescope related_files<CR>")
 vim.keymap.set("n", "<leader>E", ":e#<CR>")
 vim.keymap.set("n", "<leader>src", ":%s///gc<Left><Left><Left><Left>")
 vim.keymap.set("v", "<leader>srv", ":s///gc<Left><Left><Left><Left>")
 vim.keymap.set("n", "<leader>a", ":set relativenumber!<CR>")
+vim.keymap.set("n", "<leader>ob", ":ObsidianNew")
 vim.keymap.set("n", "q", "<Nop>")
 vim.keymap.set("n", "Q", "<Nop>")
 vim.g['test#strategy'] = 'vimux'
 vim.g.VimuxOrientation = 'h'
 vim.g.VimuxCloseOnExit = 1
-vim.g.VimuxHeight = 30 
+vim.g.VimuxHeight = 30
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 vim.cmd [[autocmd VimResized * wincmd =]]
