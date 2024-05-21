845994ad2f518081a9609ae8972e231f45956fa2
Garrett <duldonkulus@gmail.com>
2024-05-20 22:54:02 -0500

    Bug fix for vimux

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,7 +53,7 @@ vim.keymap.set("n", "Q", "<Nop>")
 vim.g['test#strategy'] = 'vimux'
 vim.g.VimuxOrientation = 'h'
 vim.g.VimuxCloseOnExit = 1
-vim.g.VimuxHeight = 30
+vim.g.VimuxHeight = "30"
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 vim.cmd [[autocmd VimResized * wincmd =]]
