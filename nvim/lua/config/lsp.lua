1a254fb7eb4db73344d74a31682dfff117f7fe9d
Garrett Julaka <garrett@cuttime.net>
2025-03-06 09:21:38 -0600

    Update lexical and power 9k

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -29,7 +29,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 
 local lspconfig = require('lspconfig')
-local configs = require("lspconfig.configs")
 local lsp_defaults = lspconfig.util.default_config
 
 lsp_defaults.capabilities = vim.tbl_deep_extend(
@@ -53,27 +52,39 @@ lsp_defaults.capabilities = vim.tbl_deep_extend(
 -- 	},
 -- })
 
+-- local lexical_config = {
+-- 	filetypes = { "elixir", "eelixir", "heex" },
+-- 	cmd = { "/Documents/scratch/git/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+-- 	settings = {},
+-- }
+
+-- if not configs.lexical then
+-- 	configs.lexical = {
+-- 		default_config = {
+-- 			filetypes = lexical_config.filetypes,
+-- 			cmd = lexical_config.cmd,
+-- 			root_dir = function(fname)
+-- 				return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+-- 			end,
+-- 			-- optional settings
+-- 			settings = lexical_config.settings,
+-- 		},
+-- 	}
+-- end
+--
 local lexical_config = {
 	filetypes = { "elixir", "eelixir", "heex" },
-	cmd = { "/Documents/scratch/git/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+	cmd = { "/Users/gjulaka/Documents/scratch/git/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
 	settings = {},
 }
 
-if not configs.lexical then
-	configs.lexical = {
-		default_config = {
-			filetypes = lexical_config.filetypes,
-			cmd = lexical_config.cmd,
-			root_dir = function(fname)
-				return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-			end,
-			-- optional settings
-			settings = lexical_config.settings,
-		},
-	}
-end
-
-lspconfig.lexical.setup({})
+lspconfig.lexical.setup({
+	filetypes = lexical_config.filetypes,
+	cmd = lexical_config.cmd,
+	root_dir = function(fname)
+		return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.cwd()
+	end
+})
 
 lspconfig.lua_ls.setup({
 	settings =
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -18,16 +18,23 @@ alias cat='bat'
 DENO_INSTALL="/Users/gjulaka/.deno"
 
 PROMPT_EOL_MARK=''
+
+export ASDF_DATA_DIR=$HOME/.asdf
+
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
 export PATH="$PATH:/opt/nvim-linux64/bin"
 export PATH="$DENO_INSTALL/bin:$PATH"
 export PATH="$PATH:$HOME/.cargo/bin"
 export PATH="$PATH:"/usr/local/lib/node_modules/node/bin"";
 export PATH=$PATH:~/.local/bin
+
 export ELIXIR_ERL_OPTIONS="-kernel shell_history enabled"
 export BAT_THEME="gruvbox-dark"
 export GIT_MIT_AUTHORS_TIMEOUT="10080"
 export EDITOR="nvim"
+
+
 eval "$(direnv hook zsh)"
+eval "$(atuin init zsh --disable-up-arrow)"
 eval "$(zoxide init zsh)"
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-# source ~/powerlevel10k/powerlevel10k.zsh-theme
